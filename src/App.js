39b9a976b5be9148a998e30934c7de856a9bb5a5
aali111 <aali@comrex.ch>
2021-12-29 15:45:46 +0500

    cmd initials

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World</p>
+        <p>CMD Initials</p>
       </header>
     </div>
   );
